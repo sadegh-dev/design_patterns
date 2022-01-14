4e500a8dffcc930b3083b0f9c7cc1ca9e2f1b201
charlie <sadegh.rez@gmail.com>
2022-01-14 21:27:59 +0330

    complete singleton

---
+++ singleton.py
@@ -0,0 +1,22 @@
+
+class Singleton(type):
+    _instance = None
+
+    def __call__(self, *args, **kwargs):
+        if self._instance is None :
+            self._instance = super().__call__()
+        return self._instance
+
+
+class Db( metaclass = Singleton ) :
+    pass
+
+d1 = Db()
+d2 = Db()
+d3 = Db()
+
+print(id(d1))
+print(id(d2))
+print(id(d3))
+
+
