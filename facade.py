4cf0c2a30231e6aca1c420adfbe3be5dfbd382bb
charlie <sadegh.rez@gmail.com>
2022-01-14 22:14:38 +0330

    facade- finish monitoring class

--- facade.py
+++ facade.py
@@ -3,3 +3,11 @@ class Monitoring():
     def __init__(self):
         print(":: Welcome to monitoring ::")
 
+    def statistics(self):
+        self.user = MyUser()
+        self.viewer = Viewer()
+        self.transaction = Transaction()
+
+        self.user.newusers()
+        self.viewer.popularpages()
+        self.transaction.most()
