b4c8cf2f5f47731c97ad51763b991e7cccb82e94
sadegh-dev <sadegh.rez@gmail.com>
2022-04-26 23:47:06 +0430

    #proxy create check_admin

--- proxy.py
+++ proxy.py
@@ -1,3 +1,11 @@
 class ConnectToDb:
     def __init__(self):
         print('Connect to database ...')
+
+
+class Proxy :
+    def check_admin(self,is_admin):
+        if is_admin :
+            c1 = ConnectToDb()
+            return c1
+        
