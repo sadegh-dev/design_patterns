df30386335293d9a7d6452a84559f0a9d4e449b9
sadegh-dev <sadegh.rez@gmail.com>
2022-04-27 21:27:30 +0430

    #iterator /for in def __next__

--- iterator.py
+++ iterator.py
@@ -1,15 +1,14 @@
 class iterator:
     
-    def __init__(self, index, step):
+    def __init__(self, index, step=1):
         self.index = index
         self.step = step
     
     def __next__(self):
         if self.index == 0 :
             return None
-
-
-
+        for x in range(0, self.index, -self.step ):
+            pass
 
 
 
